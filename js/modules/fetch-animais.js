7be749af65f3bcd6dfef4af36255805f9c36c1f8
felipe.dss17@gmail.com <felipe.dss17@gmail.com>
2022-01-28 11:43:18 -0300

    refactor anima numeros finalizado

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,4 +1,4 @@
-import initAnimaNumeros from "./anima-numeros.js";
+import AnimaNumeros from "./anima-numeros.js";
 
 function createAnimal(animal) {
   const div = document.createElement("div");
@@ -19,7 +19,12 @@ export default function initFetchAnimais() {
         numerosGrid.appendChild(divAnimal);
       });
 
-      initAnimaNumeros();
+      const animaNumeros = new AnimaNumeros(
+        "[data-numero]",
+        ".numeros",
+        "ativo",
+      );
+      animaNumeros.init();
     } catch (erro) {
       console.log(erro);
     }
