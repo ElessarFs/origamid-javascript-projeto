a7f9b251cdd573174b15b261842949ed5ad8171a
felipe.dss17@gmail.com <felipe.dss17@gmail.com>
2022-02-02 23:04:01 -0300

    scroll animacao refeito

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -2,26 +2,41 @@ export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.metadeTamanhoTela = window.innerHeight * 0.6;
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const distTop =
-        section.getBoundingClientRect().top - this.metadeTamanhoTela;
-      const isSectionVisible = distTop < 0;
-      if (isSectionVisible) {
-        section.classList.add("ativo");
-      } else if (section.classList.contains("ativo")) {
-        section.classList.remove("ativo");
+  // pega a distancia de cada item em relação ao topo do site
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.metadeTamanhoTela),
+      };
+    });
+  }
+
+  // verifica a distancia de cada objeto em relação ao scroll do site
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add("ativo");
+      } else if (item.element.classList.contains("ativo")) {
+        item.element.classList.remove("ativo");
       }
     });
   }
 
   init() {
     if (this.sections.length) {
-      window.addEventListener("scroll", this.animaScroll);
-      this.animaScroll();
+      this.getDistance();
+      window.addEventListener("scroll", this.checkDistance);
+      this.checkDistance();
     }
+    return this;
+  }
+
+  stop() {
+    window.removeEventListener("scroll", this.checkDistance);
   }
 }
