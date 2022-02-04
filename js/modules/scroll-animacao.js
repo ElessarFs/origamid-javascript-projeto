024085a1349e0ba0025ae5586ad44a2242f9e236
felipe.dss17@gmail.com <felipe.dss17@gmail.com>
2022-02-03 23:00:46 -0300

    um exemplo de como passar argumetentos para funções debounce

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -4,7 +4,10 @@ export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.metadeTamanhoTela = window.innerHeight * 0.6;
-    this.checkDistance = debounce(this.checkDistance.bind(this), 100);
+    this.checkDistance = debounce(
+      this.checkDistance.bind(this, "flksdja", "lkdasjlkdsj"),
+      100,
+    );
   }
 
   // pega a distancia de cada item em relação ao topo do site
@@ -19,8 +22,8 @@ export default class ScrollAnima {
   }
 
   // verifica a distancia de cada objeto em relação ao scroll do site
-  checkDistance() {
-    console.log("teste");
+  checkDistance(teste, teste2) {
+    console.log(teste, teste2);
     this.distance.forEach((item) => {
       if (window.pageYOffset > item.offset) {
         item.element.classList.add("ativo");
